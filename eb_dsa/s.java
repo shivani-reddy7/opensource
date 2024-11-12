f5c9e1c75084ca35a8852ad263bc0955a24c44e2
shivani-reddy7 <238r1a05r7@cmrec.ac.in>
2024-11-12 10:41:07 +0530

    Create s.java

---
+++ eb_dsa/s.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class a{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int x=sc.nextInt();
+    if(x%2==0){
+      Sytsem.out.println("even");
+    }
+    else{
+      System.out.println("odd");
+    }
+  }
+}
